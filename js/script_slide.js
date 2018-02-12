aa0df92e24bd30d7a22537b8297fbc26cc878116
Flutkov <34234648+Flutkov@users.noreply.github.com>
2018-02-13 01:13:06 +0600

    +

--- js/script_slide.js
+++ js/script_slide.js
@@ -1,3 +1,10 @@
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+  });
+
 // скролл
     $(document).ready(function() {
    $('a[href^="#_"]').on('click',function () {
